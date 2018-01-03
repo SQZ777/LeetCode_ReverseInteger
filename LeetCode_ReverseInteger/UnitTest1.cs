d0eb75ee2ad6fe722bad84fb3adaec47fbc3e748
sqz777 <sqz777isme@gmail.com>
2018-01-03 23:21:23 +0800

    fail test case : input 1

--- LeetCode_ReverseInteger/UnitTest1.cs
+++ LeetCode_ReverseInteger/UnitTest1.cs
@@ -16,7 +16,13 @@ namespace LeetCode_ReverseInteger
         [TestMethod]
         public void Input_0_Should_Be_1()
         {
-            Assert.AreEqual(1,Solution.FirstMissingPositive(new int[]{1}));
+            Assert.AreEqual(1, Solution.FirstMissingPositive(new int[] { 0 }));
+        }
+
+        [TestMethod]
+        public void Input_1_Should_Be_2()
+        {
+            Assert.AreEqual(2, Solution.FirstMissingPositive(new int[] { 1 }));
         }
 
     }
